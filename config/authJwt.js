74d7cca0422b3561ef213f0d29210addb72c2ab3
zaidi201 <zaidjb26@gmail.com>
2023-04-17 02:26:29 -0400

    config file deleted

--- config/authJwt.js
+++ config/authJwt.js
@@ -1,16 +1,15 @@
-const authConfig = require("./authConfig");
 const jwt = require("jsonwebtoken");
 const { AuthenticationError } = require("apollo-server-express");
+require("dotenv").config();
 
 exports.getUser = async (req) => {
   const token = req.headers["x-token"];
 
   if (token) {
     try {
-      return await jwt.verify(token, authConfig.secret);
+      return await jwt.verify(token, process.env.secret);
     } catch (e) {
       throw new AuthenticationError("your session expired");
     }
   }
-  //throw new AuthenticationError("your session expired in index");
 };
--- config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  HOST: "localhost",
-  USER: "postgres",
-  PASSWORD: "12345",
-  DB: "test4",
-  dialect: "postgres",
-};
